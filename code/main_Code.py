9236dde4a8b8fcd6251e4fc7deaae8ceabbc29bb
lprzem01 <156013106+lprzem01@users.noreply.github.com>
2024-02-25 13:03:55 +0000

    commit all changes

--- code/main_Code.py
+++ code/main_Code.py
@@ -1,19 +1,28 @@
 import numpy as np
 
 def numbers_sum():
+    """funtion that takes in a txt file of numbers separated by a comma and returns the sum of them in a results file. A defult file is provided if the user chooses not to provide one. """
+    #determine if the defoult file is used
     user_input = input("Do you want to use a dataset different to defoult? (yes/no)")
+    #make user input not case sensitive
+    user_input = user_input.lower()
+    #check if correct input was gived    
+    while user_input !="yes" and user_input !="no":
+        #if the correct input is not provided keep asking user to provide correct input with a prompt to clarify the correct input
+        user_input = input("Ups I did not catch that, make sure you only type in yes or no ")
+    #if initial input is yes ask user to provide their own datafile
     if user_input == "yes":
-        data_file = input("Please provide directory for your data file")
-    elif user_input == "no":
-        data_file = "data/numbers.txt"
-    else:
-        print("ups I did't catch that.Try again and remeber to only input yes/no")
-        user_input = input("Do you want to use a dataset different to defoult? (yes/no)")
-    with open(data_file) as f:
-        dataset = np.genfromtxt(f, delimiter=",")
+        data_file = input("Please provide directory for your data file ")
+    # if the user input is no the defoult data file is set
+    if user_input == "no":
+        data_file = "data/numbers.txt" 
+    #open the datafile (either user defined or defoult)
+    with open(data_file) as f: 
+        #read the file into a dataset with the delimiter set to a comma 
+        dataset = np.genfromtxt(f, delimiter=",") 
+    #set the result to be the sum of the numbers in the dataset
     result = np.sum(dataset)
+    #store the results in a file in the results folder
+    with open(r"results/example_results", "w") as f:
+        f.write(str(result))
     return str(result)
-
-
-with open(r"results/example_results", "w") as f:
-    f.write(numbers_sum())
--- code/test_code.py
+++ code/test_code.py
@@ -1,12 +1,24 @@
-
+from main_Code import numbers_sum
 import pytest
 
-def func(x):
-    return x + 1
+def test_numbers_sum(monkeypatch):
+    """test to check if defoult data returns correct number"""
+    # Monkeypatch the "input" function to return "no".
+    monkeypatch.setattr('builtins.input', lambda _: "no")
 
+    # Call the function and assert the output.
+    assert numbers_sum() == "781.0"
 
-def test_answer():
-    assert numbers_sum("no") == 781.0
-    
+def test_data(monkeypatch):
+    """test to check if test data returns correct output"""
+    # Monkeypatch the "input" function to return "no".
+    monkeypatch.setattr('builtins.input', lambda _: "yes")
+    monkeypatch.setattr('builtins.input', lambda _: "data/test_data")
 
-retcode = pytest.main()
+    # Call the function and assert the output.
+    assert numbers_sum() == "2"
+
+# Run the test
+if __name__ == "__main__":
+    pytest.main()
+
